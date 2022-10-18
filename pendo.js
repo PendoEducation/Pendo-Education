e2aea006b7aa49c28af985816ed9feea88479765
PendoEducation <92550747+PendoEducation@users.noreply.github.com>
2022-10-17 20:54:00 -0400

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -6,11 +6,13 @@
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
-        // Call this whenever information about your visitors becomes available
+        // This function creates anonymous visitor IDs in Pendo unless you change the visitor id field to use your app's values
+        // This function uses the placeholder 'ACCOUNT-UNIQUE-ID' value for account ID unless you change the account id field to use your app's values
+        // Call this function after users are authenticated in your app and your visitor and account id values are available
         // Please use Strings, Numbers, or Bools for value types.
         pendo.initialize({
             visitor: {
-                id:              'VISITOR-UNIQUE-ID'   // Required if user is logged in
+                id:              'VISITOR-UNIQUE-ID'   // Required if user is logged in, default creates anonymous ID
                 // email:        // Recommended if using Pendo Feedback, or NPS Email
                 // full_name:    // Recommended if using Pendo Feedback
                 // role:         // Optional
@@ -20,7 +22,7 @@
             },
 
             account: {
-                id:           'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback
+                id:           'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
                 // name:         // Optional
                 // is_paying:    // Recommended if using Pendo Feedback
                 // monthly_value:// Recommended if using Pendo Feedback
